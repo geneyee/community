6fd6f6a9af64c87aaced409d3d04465ed9c6c832
geneyee <geneyeey@gmail.com>
2023-12-27 13:51:21 +0900

    commit

--- src/main/java/com/dev/community/exception/DataNotFoundException.java
+++ src/main/java/com/dev/community/exception/DataNotFoundException.java
@@ -3,6 +3,10 @@ package com.dev.community.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import lombok.extern.log4j.Log4j2;
+
+
+@Log4j2
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "entity not found")
 public class DataNotFoundException extends RuntimeException{
 
@@ -13,6 +17,7 @@ public class DataNotFoundException extends RuntimeException{
 	
 	public DataNotFoundException(String message) {
 		super(message);
+		log.info("DataNotFoundException error message => {}", message);
 	}
 
 }
